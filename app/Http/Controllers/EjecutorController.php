d4c949262036d93b3b6a50f7388e4fbe5ec634b0
danilopez97 <estu97dani@gmail.com>
2019-09-22 12:58:46 -0600

    funciones listar,registrar, actualizar ejecutor

--- app/Http/Controllers/EjecutorController.php
+++ app/Http/Controllers/EjecutorController.php
@@ -2,9 +2,87 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Ejecutor;
+use Illuminate\Support\Facades\DB;
 
 class EjecutorController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        //
+       //if(!$request->ajax()) return redirect('/');
+
+       $buscar = $request->buscar;
+       $criterio = $request->criterio;
+
+       if ($buscar==''){
+         $ejecutores = Ejecutor::join('cargoejecutores','ejecutores.idcargoejecutor','=','cargoejecutores.id')
+         ->select('ejecutores.id','ejecutores.idcargoejecutor','ejecutores.nombre','ejecutores.direccion','cargoejecutores.nombre as nombre_cargoejecutor','ejecutores.estado')
+         ->orderBy('ejecutores.id', 'desc')->paginate(3);
+        }
+        else{
+            $ejecutores = Ejecutor::join('cargoejecutores','ejecutores.idcargoejecutor','=','cargoejecutores.id')
+            ->select('ejecutores.id','ejecutores.idcargoejecutor','ejecutores.nombre','ejecutores.direccion','cargoejecutores.nombre as nombre_cargoejecutor','ejecutores.estado')
+            ->where('ejecutores.'.$criterio, 'like', '%'. $buscar . '%')
+            ->orderBy('ejecutores.id', 'desc')->paginate(3);
+
+            //$ejecutores = Ejecutor::where($criterio, 'like', '%'. $buscar . '%')->orderBy('id', 'desc')->paginate(3);
+        }
+       //$ejecutores=Ejecutor::paginate(2);
+        //return $ejecutores;
+        return [
+            'pagination' => [
+                'total'        => $ejecutores->total(),
+                'current_page' => $ejecutores->currentPage(),
+                'per_page'     => $ejecutores->perPage(),
+                'last_page'    => $ejecutores->lastPage(),
+                'from'         => $ejecutores->firstItem(),
+                'to'           => $ejecutores->lastItem(),
+            ],
+            'ejecutores' => $ejecutores
+        ];
+    }
+
+
+
+    public function store(Request $request)
+    {
+        if(!$request->ajax()) return redirect('/');
+
+        $ejecutor=new Ejecutor();
+        $ejecutor->idcargoejecutor=$request->idcargoejecutor;
+        $ejecutor->nombre=$request->nombre;
+        $ejecutor->direccion=$request->direccion;
+        $ejecutor->estado='1';
+        $ejecutor->save();
+    }
+
+    public function update(Request $request)
+    {
+        if(!$request->ajax()) return redirect('/');
+        $ejecutor=Ejecutor::findOrFail($request->id);
+        $ejecutor->idcargoejecutor=$request->idcargoejecutor;
+        $ejecutor->nombre=$request->nombre;
+        $ejecutor->direccion=$request->direccion;
+        $ejecutor->estado='1';
+        $ejecutor->save();
+    }
+
+    public function desactivar(Request $request)
+    {
+        if(!$request->ajax()) return redirect('/');
+        $ejecutor=Ejecutor::findOrFail($request->id);
+        $ejecutor->estado='0';
+        $ejecutor->save();
+    }
+
+    public function activar(Request $request)
+    {
+        if(!$request->ajax()) return redirect('/');
+        $ejecutor=Ejecutor::findOrFail($request->id);
+        $ejecutor->estado='1';
+        $ejecutor->save();
+    }
 }
